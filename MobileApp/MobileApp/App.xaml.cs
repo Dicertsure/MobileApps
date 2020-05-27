f5db04f11f7f4b86193b65179462b21aa934f11e
etimdivine.agborokon <divine.agbor@certsure.com>
2020-05-27 15:05:53 +0100

    prepare solution for Appcenter

--- MobileApp/MobileApp/App.xaml.cs
+++ MobileApp/MobileApp/App.xaml.cs
@@ -4,6 +4,13 @@ using Xamarin.Forms.Xaml;
 using MobileApp.Services;
 using MobileApp.Views;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace MobileApp
 {
     public partial class App : Application
@@ -19,6 +26,10 @@ namespace MobileApp
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=da4dfdfa-636f-41a2-8ee8-c7a4ed783544;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
